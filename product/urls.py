12d74a873b028b227ddd5132db91bf231711152c
JiMarchel <jmarchel100@gmail.com>
2026-02-06 13:29:11 +0700

    feat: add product management routes to urls.py

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product/form/", views.product_form, name="product_form"),
+    path("product/create/", views.product_create, name="product_create"),
+    path(
+        "product/<int:pk>/delete/confirm/",
+        views.product_delete_confirm,
+        name="product_delete_confirm",
+    ),
+    path("product/<int:pk>/delete/", views.product_delete, name="product_delete"),
+    path("product/<int:pk>/edit/", views.product_edit_form, name="product_edit_form"),
+    path("product/<int:pk>/update/", views.product_update, name="product_update"),
 ]
